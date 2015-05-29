03d86434d3e90703c2fc14f7a37cc815fc2dcdcb
bruney <bruneyapps@gmail.com>
2015-05-29 17:43:14 +0200

    Primer controlador y modelo

---
+++ photos/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class Photo(models.Model):
+
+    COPYRIGHT = 'RIG'
+    COPYLEFT = 'LEF'
+    CREATIVE_COMMONS = 'CC'
+
+    LICENSES = (
+        (COPYRIGHT, 'Copyright'),
+        (COPYLEFT, 'Copyleft'),
+        (CREATIVE_COMMONS, 'Creative Commons'),
+    )
+
+    name = models.CharField(max_length=150)
+    url = models.URLField()
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True) # Pone la fecha automáticamente cuando se crea
+    modified_on = models.DateTimeField(auto_now=True) # Pone la fecha automáticamente cuando se guarda
+    # choices es una tupla que desplegará luego en la web todas las opciones.
+    # ponemos una tupla para que sea inmutable. Podríamos haber utilizado un diccionario.
+    license = models.CharField(max_length=3, choices=LICENSES)
