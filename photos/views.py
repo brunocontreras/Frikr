6e35148ac547fb897e17794c919d174907ea8026
bruney <bruneyapps@gmail.com>
2015-05-29 19:03:44 +0200

    Ordenación y filtrado de las fotos

--- photos/views.py
+++ photos/views.py
@@ -5,8 +5,8 @@ from photos.models import Photo
 # Create your views here.
 
 def home(request):
-    photos = Photo.objects.all()
+    photos = Photo.objects.all().order_by('-created_on')
     context = {
-        'photo_list': photos,
+        'photo_list': photos[:5],
     }
     return render(request, 'photos/home.html', context)
