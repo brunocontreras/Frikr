f05ad29e4c582d036c5ece0eac4e2e4103a6dfd3
Bruno Contreras Chicote <bruneyapps@gmail.com>
2015-06-11 19:37:06 +0200

    Columnas en la lista de fotos en el admin

--- photos/admin.py
+++ photos/admin.py
@@ -3,5 +3,12 @@ from photos.models import Photo
 
 # Register your models here.
 
+class PhotoAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'owner', 'license', 'visibility')
+    list_filter = ('license', 'visibility', 'created_on')
+    search_fields = ('name', 'description')
+
+
 # Habilita la entidad photo en el administrador de Django
-admin.site.register(Photo)
+admin.site.register(Photo, PhotoAdmin)
