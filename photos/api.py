41c70403bc77f9c29ba2a4e9d636e56121872057
Bruno Contreras Chicote <bruneyapps@gmail.com>
2015-06-11 21:35:58 +0200

    Uso de APIView para que la serialización la haga automáticamente

--- photos/api.py
+++ photos/api.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.models import User
-from django.http import HttpResponse
-from django.views.generic import View
 from photos.serializers import UserSerializer
-from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
-class UserListAPI(View):
+class UserListAPI(APIView):
 
     def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
-        renderer = JSONRenderer()
-        data = renderer.render(serializer.data)
-        return HttpResponse(data)
+        return Response(serializer.data)
