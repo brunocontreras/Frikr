064461417a864a52a7727b410f1350a40d967e52
Bruno Contreras Chicote <bruneyapps@gmail.com>
2015-06-13 13:19:33 +0200

    Nueva aplicación Files para subir archivos. Modelo creado

---
+++ files/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.conf import settings
+
+UPLOAD_FOLDER = getattr(settings, 'UPLOAD_FOLDER', 'uploads')
+
+class File(models.Model):
+
+    file = models.FileField(upload_to=UPLOAD_FOLDER)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User)
