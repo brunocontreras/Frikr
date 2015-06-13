995e8d61b89bfea943988560a41b859053dced02
Bruno Contreras Chicote <bruneyapps@gmail.com>
2015-06-13 09:37:54 +0200

    Eliminación del validador en el formulario (debería haberlo quitado antes)

--- photos/forms.py
+++ photos/forms.py
@@ -14,13 +14,3 @@ class PhotoForm(forms.ModelForm):
     class Meta:
         model = Photo
         exclude = ('owner',)
-
-    # Limpia el formulario, no sólo valida sino que normaliza los valores recibidos.
-    # se llama al clean del padre porque ya hace una limpieza.
-    def clean(self):
-        cleaned_data = super(PhotoForm, self).clean()
-        description = cleaned_data.get('description', '')
-        for badword in BADWORDS:
-            if badword.lower() in description.lower():
-                raise forms.ValidationError(badword + u" no está admitida")
-        return cleaned_data
